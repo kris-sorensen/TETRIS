a2dac00406b23e06600e8094b2612d63ea990541
Kris Sorensen <Krismsorensen@gmail.com>
2022-02-21 21:34:39 -0500

    added music on loop.

--- components/blockRain.js/blockrain.jsx
+++ components/blockRain.js/blockrain.jsx
@@ -62,7 +62,7 @@
 
 
 
-//* Supoort Functions. 
+
 
 
 
@@ -78,8 +78,13 @@
         ---->
             --->  <BlockRain></BlockRain> ---> {for loop to create matrice, state width & length}, () Methods: {drop piece, }
 
-            --->  <Main></Main> --> {Logo/Title, play btn. stats btn. high scores btn. about btn. 2p/ 1p slider. }
-*/
+            --->  <Container`></Container`> --> {Logo/Title, play btn. stats btn. high scores btn. about btn. 2p/ 1p slider. }
+
+                    --->
+                        <Logo></Logo>
+*/                 
+
+import React, { Component } from 'react';
 
 
 
@@ -98,6 +103,10 @@ export default App;
 
 ////////////
 
+import React, { Component } from 'react';
+import BlockRain from './blockrain'
+import Container from './container'
+
 
 class Canvas extends Component {
     constructor(props) {
@@ -106,9 +115,10 @@ class Canvas extends Component {
     state = {  }
     render() { 
         return (
-            <> </>
-            <BlockRain></BlockRain>
-            <Contaienr></Contaienr>
+            <> 
+                <BlockRain></BlockRain>
+                <Container></Container>
+            </>
           );
     }
 }
@@ -118,29 +128,96 @@ export default Canvas;
 
 
 ///////////
+import React, { Component } from 'react';
 
 
-
-class BlackRain extends Component {
+class BlockRain extends Component {
     state = {  } 
+    // create matrice and render in grid style. Take the width of the screen and divide it by X to get square sizes. and set in style
     render() { 
         return ();
     }
+
+    //Methods:
+
+    // drop
+    // find spot
+    // choose piece
 }
+
+// self starting function that gets loop started
  
-export default BlackRain;
+export default BlockRain;
 
 
 
 
 ///////////
 
+import React, { Component } from 'react';
+import Logo from './logo'
 
-class Main extends Component {
+
+class Container extends Component {
     state = {  } 
     render() { 
-        return ();
+        return (
+            <>
+                <Logo></Logo>
+                <div className="singleMultiSlider"></div>
+                <div className="play"></div> // triggers a popup that lets use choose your name or login with 4 digit code so you can use your same tag.
+                <div className="topScores"></div> // goes to an arcade like top scores page. 
+                <div className="records"></div> Endless scroll
+            </>
+        );
+    }       
+}
+ 
+export default Container;
+
+
+
+
+///////////////
+import React, { Component } from 'react';
+
+
+class Logo extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <img src="" alt="Logo" srcset="" />
+        );
     }
 }
  
-export default Main;
+export default Logo;
+
+
+
+
+
+//.............. Other Pages/Popups
+
+
+//* TOP SCORES
+
+
+
+//* User Stats
+
+
+//* Choose your name:
+        // Have a ready button you click and turns bright with sound when you click ready.
+        // searches to make sure the name is available that you choose.
+
+
+
+/********************* BACKEND */
+
+
+// Giant object that stores user names and passcodes. and past results w/ detail of who they played. how the other person did, date/&Time, time alive, level, score, ability to sort score based on thes factors?
+    // Mehods. search find retrieve data, delete user, if exists or wrong passcode statements. is new user message. and display create user.
+// disregard games that were ended under 1 min. 1
+
+
